92baf09b854e8fd5a01ed23ae3b634f48fb49385
bun919tw <bun919tw@gmail.com>
2018-01-23 23:18:45 +0800

    Replace extract function with assembly
    * Also, change merkle hash function to sha3.

--- plasma.sol
+++ plasma.sol
@@ -54,12 +54,7 @@ contract PlasmaChainManager {
     using MinHeapLib for MinHeapLib.Heap;
 
     bytes constant PersonalMessagePrefixBytes = "\x19Ethereum Signed Message:\n68";
-    uint32 constant blockNumberLength = 4;
-    uint32 constant previousHashLength = 32;
-    uint32 constant merkleRootLength = 32;
-    uint32 constant sigRLength = 32;
-    uint32 constant sigSLength = 32;
-    uint32 constant sigVLength = 1;
+
     uint32 constant blockHeaderLength = 133;
     uint32 constant transactionLength = 90;
 
@@ -107,46 +102,6 @@ contract PlasmaChainManager {
         depositCounter = 0;
     }
 
-    function extract32(bytes data, uint pos)
-        pure
-        internal
-        returns (bytes32 result)
-    {
-        for (uint256 i = 0; i < 32; i++) {
-            result ^= (bytes32(0xff00000000000000000000000000000000000000000000000000000000000000)&data[i+pos])>>(i*8);
-        }
-    }
-
-    function extract20(bytes data, uint pos)
-        pure
-        internal
-        returns (bytes20 result)
-    {
-        for (uint256 i = 0; i < 20; i++) {
-            result ^= (bytes20(0xff00000000000000000000000000000000000000)&data[i+pos])>>(i*8);
-        }
-    }
-
-    function extract4(bytes data, uint pos)
-        pure
-        internal
-        returns (bytes4 result)
-    {
-        for (uint256 i = 0; i < 4; i++) {
-            result ^= (bytes4(0xff000000)&data[i+pos])>>(i*8);
-        }
-    }
-
-    function extract1(bytes data, uint pos)
-        pure
-        internal
-        returns (bytes1 result)
-    {
-        for (uint256 i = 0; i < 1; i++) {
-            result ^= (bytes1(0xff)&data[i+pos])>>(i*8);
-        }
-    }
-
     function setOperator(address operator, bool status)
         public
         returns (bool success)
@@ -161,40 +116,48 @@ contract PlasmaChainManager {
     function submitBlockHeader(bytes header) public returns (bool success) {
         require(operators[msg.sender]);
         require(header.length == blockHeaderLength);
-        uint32 blockNumber = uint32(extract4(header, 0));
-        bytes32 previousHash = extract32(header, 4);
-        bytes32 merkleRoot = extract32(header, 36);
-        bytes32 sigR = extract32(header, 68);
-        bytes32 sigS = extract32(header, 100);
-        uint8 sigV = uint8(extract1(header, 132));
+
+        bytes4 blockNumber;
+        bytes32 previousHash;
+        bytes32 merkleRoot;
+        bytes32 sigR;
+        bytes32 sigS;
+        bytes1 sigV;
+        assembly {
+            let data := add(header, 0x20)
+            blockNumber := mload(data)
+            previousHash := mload(add(data, 4))
+            merkleRoot := mload(add(data, 36))
+            sigR := mload(add(data, 68))
+            sigS := mload(add(data, 100))
+            sigV := mload(add(data, 132))
+            if lt(sigV, 27) { sigV := add(sigV, 27) }
+        }
 
         // Check the block number.
-        require(blockNumber == lastBlockNumber + 1);
+        require(uint8(blockNumber) == lastBlockNumber + 1);
 
         // Check the signature.
         bytes32 blockHash = keccak256(PersonalMessagePrefixBytes, blockNumber,
             previousHash, merkleRoot);
-        if (sigV < 27) {
-            sigV += 27;
-        }
-        address signer = ecrecover(blockHash, sigV, sigR, sigS);
+        address signer = ecrecover(blockHash, uint8(sigV), sigR, sigS);
         require(msg.sender == signer);
 
         // Append the new header.
         BlockHeader memory newHeader = BlockHeader({
-            blockNumber: blockNumber,
+            blockNumber: uint8(blockNumber),
             previousHash: previousHash,
             merkleRoot: merkleRoot,
             r: sigR,
             s: sigS,
-            v: sigV
+            v: uint8(sigV)
         });
-        headers[blockNumber] = newHeader;
+        headers[uint8(blockNumber)] = newHeader;
 
         // Increment the block number by 1 and reset the deposit counter.
         lastBlockNumber += 1;
         depositCounter = 0;
-        HeaderSubmittedEvent(signer, blockNumber);
+        HeaderSubmittedEvent(signer, uint8(blockNumber));
         return true;
     }
 
@@ -233,8 +196,11 @@ contract PlasmaChainManager {
         require(isValidProof(header.merkleRoot, targetTx, proof));
 
         // Check if the transaction owner is the sender.
-        address txOwner = address(extract20(targetTx, 5));
-        require(txOwner == msg.sender);
+        bytes20 txOwner;
+        assembly {
+            txOwner := mload(add(add(targetTx, 5), 0x20))
+        }
+        require(address(txOwner) == msg.sender);
 
         // Check if the withdrawal exists.
         withdrawalId = uint256(blockNumber) * 1000000000 + uint256(txIndex) * 10000;
@@ -244,7 +210,7 @@ contract PlasmaChainManager {
         // Construct a new withdrawal and add its ID to the heap.
         record.blockNumber = blockNumber;
         record.txIndex = txIndex;
-        record.beneficiary = txOwner;
+        record.beneficiary = address(txOwner);
         record.status = WithdrawStatus.Created;
         record.timeStarted = now;
         exits.add(withdrawalId);
@@ -276,11 +242,17 @@ contract PlasmaChainManager {
         WithdrawRecord storage record = withdrawRecords[withdrawalId];
         require(record.blockNumber > 0);
 
-        uint32 utxoBlockNumber = uint32(extract4(targetTx, 0));
-        uint32 utxoTxIndex = uint32(extract1(targetTx, 4));
+        bytes4 utxoBlockNumber;
+        bytes1 utxoTxIndex;
+        assembly {
+            let data := add(targetTx, 0x20)
+            utxoBlockNumber := mload(data)
+            utxoTxIndex := mload(add(data, 4))
+        }
 
         // The transaction spends the given withdrawal on plasma chain.
-        if (record.blockNumber == utxoBlockNumber && record.txIndex == utxoTxIndex) {
+        if (record.blockNumber == uint32(utxoBlockNumber) &&
+            record.txIndex == uint32(utxoTxIndex)) {
             record.timeEnded = now;
             record.status = WithdrawStatus.Challenged;
             WithdrawalChallengedEvent(withdrawalId);
@@ -323,14 +295,18 @@ contract PlasmaChainManager {
         internal
         returns (bool valid)
     {
-        bytes32 hash = sha256(target);
-        for (uint i = 0; i < proof.length; i += 33) {
-            uint8 flag = uint8(extract1(proof, i));
-            bytes32 sibling = extract32(proof, i + 1);
+        bytes32 hash = keccak256(target);
+        for (uint i = 32; i < proof.length; i += 33) {
+            bytes1 flag;
+            bytes32 sibling;
+            assembly {
+                flag := mload(add(proof, i))
+                sibling := mload(add(add(proof, i), 1))
+            }
             if (flag == 0) {
-                hash = sha256(sibling, hash);
+                hash = keccak256(sibling, hash);
             } else if (flag == 1) {
-                hash = sha256(hash, sibling);
+                hash = keccak256(hash, sibling);
             }
         }
         return hash == root;
